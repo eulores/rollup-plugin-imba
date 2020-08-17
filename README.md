720d107b4b9acfd63956b492e2e2e439532f033b
Andreas <43118918+eulores@users.noreply.github.com>
2020-08-17 14:29:37 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,12 @@
+# rollup-plugin-imba
+
+Use the [Imba compiler](https://v2.imba.io/) (v2) to build all your `*.imba` files from source using [Rollup](https://rollupjs.org/).
+
+## Install
+
+```sh
+# npm:
+npm install --save-dev rollup-plugin-imba
+# yarn:
+yarn --dev add rollup-plugin-imba
+```
---
+++ src/index.js
@@ -0,0 +1,50 @@
+// rollup-plugin-imba written 2020 by eulores and released under MIT license
+
+import { createFilter } from '@rollup/pluginutils';
+import { extname, relative, basename}  from 'path';
+import { compile } from 'imba/dist/compiler.js';
+
+function debug(wp, ...args) {
+  if (debug.active) console.warn(`WP${wp}:`, ...args);
+}
+
+export default function(pluginOptions={}) {
+  if (pluginOptions.debug==true) debug.active = true;
+  const filter = createFilter(pluginOptions.include, pluginOptions.exclude);
+
+  return {
+    name: 'imba',
+
+    transform ( code, id ) {
+      debug(1, "transform, before filter:", id);
+      if (!filter(id)) return null;
+      const ext = extname(id);
+      if (ext!='.imba') return null;
+      debug(2, "transform, before compile:", id);
+      const options = {
+        target: 'web',
+        format: 'esm',
+        es6: true,
+        standalone: false,
+        sourceMap: true,
+        sourceRoot: '',
+        evaling: true,
+      };
+      Object.assign(options, pluginOptions);
+      const filePath = relative(process.cwd(), id);
+      options.filename = basename(filePath);
+      options.sourcePath = filePath;
+      options.targetPath = options.sourcePath.replace(/\.imba\d?$/,'.js');
+      let {js, sourcemap} = compile(code, options);
+      // consider extracting warnings for custom display (switch of evaling!)
+      // consider extracting css as well
+      delete sourcemap.maps; // debugging leftover?
+      debug(3, "transform, after compile:", id);
+      return {
+        code: js,
+        map: sourcemap,
+        moduleSideEffects: true // further investigation needed (special treatment for imba/src/imba?)
+      };
+    }
+  };
+}
